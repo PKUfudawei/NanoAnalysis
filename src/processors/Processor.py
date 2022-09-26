5c928ed2fd8ea4847156e49c5f5af8fc4714c081
Dawei Fu <fudw@pku.edu.cn>
2022-09-27 03:36:21 +0800

    finish GenMatch and Processor

--- src/processors/Processor.py
+++ src/processors/Processor.py
@@ -21,7 +21,7 @@ class Processor(processor.ProcessorABC):
             self, events: NanoEventsArray, deltaR_cut: float=0.8,
             variables: dict={
                 'AK8jet': {'pt', 'eta', 'phi', 'mass', 'msoftdrop'},
-                'photon': {'pt', 'eta', 'phi', 'mass', 'msoftdrop'},
+                'photon': {'pt', 'eta', 'phi', 'mass'},
                 'event': {'MET_pt'},
             }, trigger: str='Photon175'
         ):
@@ -38,7 +38,7 @@ class Processor(processor.ProcessorABC):
             (muons.pt > 20) # I don't use `muon_corrected_pt` coming from ROOT.RoccoR
         )
         self.object['muon'] = muons[muon_cut]
-        event_cut *= (ak.sum(muon_cut, axis=-1)==0)
+        event_cut = event_cut * (ak.sum(muon_cut, axis=-1)==0) ## 0 muon
 
         ## Electron
         electrons = self.object['event'].Electron # (event, electron)
@@ -48,7 +48,7 @@ class Processor(processor.ProcessorABC):
             (electrons.pt > 20)
         )
         self.object['electron'] = electrons[electron_cut]
-        event_cut *= (ak.sum(muon_cut, axis=-1)==0)
+        event_cut = event_cut * (ak.sum(muon_cut, axis=-1)==0) ## 0 electron
         
         ## Photon
         photons = self.object['event'].Photon # (event, photon)
@@ -58,7 +58,7 @@ class Processor(processor.ProcessorABC):
             (abs(photons.eta) < 2.4)
         )
         self.object['photon'] = photons[photon_cut]
-        event_cut *= (ak.sum(photon_cut, axis=-1)>0)
+        event_cut = event_cut * (ak.sum(photon_cut, axis=-1)>0) ## >=1 photon
 
         ## AK8 jet
         AK8jets = self.object['event'].FatJet # (event, fatjet)
@@ -70,32 +70,30 @@ class Processor(processor.ProcessorABC):
             # Jet ID flags bit1 is loose (always false in 2017 since it does not exist), bit2 is tight, bit3 is tightLepVeto
         )
         self.object['AK8jet'] = AK8jets[AK8jet_cut]
-        event_cut *= (ak.sum(AK8jet_cut, axis=-1)>0)
+        event_cut = event_cut * (ak.sum(AK8jet_cut, axis=-1)>0) ## >=1 AK8 jet
         
         ## Photon-Jet cleaning
         photon_jet_pair = ak.cartesian({'photon': self.object['photon'], 'jet': self.object['AK8jet']}, axis=1, nested=False)
-        photon_jet_pair_index = ak.argcartesian({'photon': self.object['photon'], 'jet': self.object['AK8jet']}, axis=1, nested=False)
+        photon_jet_index_pair = ak.argcartesian({'photon': self.object['photon'], 'jet': self.object['AK8jet']}, axis=1, nested=False)
         
         photon_jet_dr = photon_jet_pair.photon.delta_r(photon_jet_pair.jet)
-        photon_jet_clean = (
-            ak.num(photon_jet_dr > deltaR_cut, axis=1)==1
-        )
-        event_cut *= (ak.sum(photon_jet_clean, axis=-1)==1)
+        photon_jet_clean = (photon_jet_dr > deltaR_cut)
+        photon_index, jet_index = photon_jet_index_pair.photon[photon_jet_clean], photon_jet_index_pair.jet[photon_jet_clean]
         
-        photon_index, jet_index = photon_jet_pair_index[photon_jet_clean].photon, photon_jet_pair_index[photon_jet_clean].jet
-        self.object['photon'] = self.object['photon'][photon_index]
-        self.object['AK8jet'] = self.object['AK8jet'][jet_index]
+        self.object['photon'] = self.object['photon'][photon_index] ## may exists repetition
+        self.object['AK8jet'] = self.object['AK8jet'][jet_index] ## may exists repetition
+        event_cut = event_cut * (ak.sum(photon_jet_clean, axis=-1)==1) ## exactly 1 pair
         
         ## Return vars of objects after pre-selection
         for obj in ['event', 'photon', 'AK8jet']:
             self.object[obj] = self.object[obj][event_cut]
-        
-        self.variables.update({
-            var: (
-                self.object[obj][var] if obj!='event' else self.object[obj][var.split('_')[0]][var.split('_')[1:]]
-            ) for (obj, var) in variables.items()
-        })
-        
+            if obj=='event':
+                self.variables.update({
+                    obj+'_'+var: self.object[obj][var.split('_')[0]][var.split('_')[1:]] for var in variables[obj]
+                })
+            else:
+                self.variables.update({obj+'_'+var: self.object[obj][var] for var in variables[obj]})
+
         return event_cut
 
     def process(self, events: NanoEventsArray):
