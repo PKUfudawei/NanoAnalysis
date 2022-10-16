1af46edf2d456a17ec6c732e1e00210aa75c67f7
Dawei Fu <fudw@pku.edu.cn>
2022-10-16 14:20:24 +0800

    update condor

--- condor/getFileList.py
+++ condor/getFileList.py
@@ -7,7 +7,7 @@ import subprocess
 
 def parse_commanline():
     parser = argparse.ArgumentParser(description='Script to check if each condor job is done')
-    parser.add_argument('-d', '--directory', help='To specify base directory', default=os.path.abspath('./datasets'))
+    parser.add_argument('-d', '--directory', help='To specify base directory', default=os.path.abspath('../datasets'))
     parser.add_argument('-t', '--type', help='To specify jobs in mc/ or data/', choices=('data', 'mc'), default='*')
     parser.add_argument('-y', '--year', help='To specify jobs in which year', default='*')
     parser.add_argument('-c', '--channel', help='To specify jobs in which channel', default='*')
@@ -15,47 +15,34 @@ def parse_commanline():
     args = parser.parse_args()
     return args
 
-def download_from_dataset_card(card_path: str, out_basedir: str):
+def filelist_from_dataset_card(card_path: str):
     channel, year, type = card_path.split('/')[-2], card_path.split('/')[-3], card_path.split('/')[-4]
-    out_basedir = os.path.join(out_basedir, type, year, channel)
     with open(card_path, 'r') as f:
         dataset = yaml.load(f, Loader=yaml.FullLoader)
 
-    paths = []
+    filelist = []
     for (k, v) in dataset.items():
-        out_dir = out_basedir + v['dataset'] if v['dataset'].startswith('/') else os.path.join(out_basedir, v['dataset'])
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
         query_str = f"\"file dataset={v['dataset']} system=rucio\""
         output = subprocess.check_output(f"/cvmfs/cms.cern.ch/common/dasgoclient -query={query_str} -json", shell=True, encoding='utf-8')
         output = json.loads(output)
         for file_info in output:
-            paths.append('root://cms-xrd-global.cern.ch/'+file_info['file'][0]['name'])
+            filelist.append('root://cms-xrd-global.cern.ch/'+file_info['file'][0]['name'])
         with open(f'./{channel}.txt', 'w') as f:
-            f.write('\n'.join(paths))
-            
-            #print(f'xrdcp {file} {out_dir}')
-            #os.system(f'xrdcp {file} {out_dir}')
-        #print()
-        #print(f'\t\tDataset of {k} is done')
-        #print()
-
+            f.write('\n'.join(filelist))
+    print(f'==> Generated {channel}.txt from {card_path}')
+    
     return True
 
 def main() -> None:
     args = parse_commanline()
     dataset_cards = '/'.join([args.directory, args.type, args.year, args.channel, args.version])+'.yaml'
     dataset_cards = set(glob.glob(dataset_cards))
-    total = len(dataset_cards)
+    print(f'==> Start generating {len(dataset_cards)} filelist(s) from {dataset_cards}')
     
-    with open('../checkoutNano/config/condor_cfg.yaml', 'r') as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    finished = 0
+    succeeded = 0
     for dataset_card in dataset_cards:
-        finished += download_from_dataset_card(card_path=dataset_card, out_basedir=config['out_dir'])
-        print('====================================================================')
-        print('==> Downloading process:\t%.1f'%(100*finished/total))
-        print('====================================================================')
+        succeeded += filelist_from_dataset_card(card_path=dataset_card)
+    print(f'==> Successfully generated {succeeded} file list(s) in total')
         
 if __name__ == "__main__":
     main()
