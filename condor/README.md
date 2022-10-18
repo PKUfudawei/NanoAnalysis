3092dbd1b2d2775744854afc81a70b2b4ef4613b
Dawei Fu <fudw@pku.edu.cn>
2022-10-18 09:27:42 +0200

    update condor_submit.py

--- condor/README.md
+++ condor/README.md
@@ -6,3 +6,7 @@ year=
 channel=
 for i in submit/$type/$year/$channel/*.submit; do condor_submit $i; done
 ```
+
+## *References*
+[CERN Batch Docs](https://batchdocs.web.cern.ch/index.html)
+[HTCondor Manual](https://htcondor.readthedocs.io/en/latest/index.html)
--- condor/condor_submit.py
+++ condor/condor_submit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
-import glob
+import subprocess
 
 def parse_commanline():
     parser = argparse.ArgumentParser(description='Script to check if each condor job is done')
@@ -16,12 +16,32 @@ def main() -> None:
     args = parse_commanline()
     ## 4506743.36 from QCD_HT1000to1500_TuneCP5
     ## 4506748.45 from QCD_HT300to500_TuneCP5
+    schedd = subprocess.check_output(f"myschedd bump", shell=True, encoding='utf-8').split("'")[1]
+    ## myschedd set $schedd
     os.system(f"""
-        ## myschedd bump
+        echo "###########################################################################"
+        echo "====> Jump to the best schedd"
+        myschedd show
+        echo "###########################################################################"
+        echo "====> Status fo current schedd"
+        condor_status -schedd {schedd}
+        echo "###########################################################################"
+        echo "====> Wanna submit condor jobs on this schedd?"
+        
+        while true          # interactive interface
+        do
+            read -n 1 -p "Press ENTER to submit condor jobs or CTRL+c to exit> " input
+            if [[ -z $input ]];then
+                break   
+            fi
+            echo ""
+        done
+        echo ""
+
         for jdl in submit/{args.type}/{args.year}/{args.channel}/{args.job}.submit
             do condor_submit $jdl
         done
-    """)
+        """)
     
 if __name__ == "__main__":
     main()
