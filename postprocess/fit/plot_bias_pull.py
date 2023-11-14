f98cc1979c2cec461cc976639a9d6ed6d2adb0f2
Dawei Fu <fudw@pku.edu.cn>
2023-11-14 14:58:55 +0800

    bias pull result

--- postprocess/fit/plot_bias_pull.py
+++ postprocess/fit/plot_bias_pull.py
@@ -1,33 +1,22 @@
 import ROOT
 import numpy as np
-import argparse, os
+import argparse, os, yaml
 
 ROOT.gROOT.SetBatch(True)
 
-
-def parse_commandline():
-    parser = argparse.ArgumentParser(description='parametric fitting')
-    parser.add_argument('-y', '--year', help='To specify which year', choices=('2016pre', '2016post', '2017', '2018', 'Run2'), default='Run2')
-    # parser.add_argument('-t', '--truth_function', help='To specify which truth function', choices=('expow2', 'invpow2'))
-    parser.add_argument('-m', '--signal_mass', help='To specify the mass of signal resonance', type=int)
-    parser.add_argument('-R', '--SR', help='To specify which signal region', choices=('SR1', 'SR2'))
-    args = parser.parse_args()
-    return args
+N_toys = 1000
+year = 'Run2'
 
 
 def plot(truth_function, signal_mass, SR, fit_function='dijet2'):
-    N_toys = 1000
     r_truth = 1
-
-    year = 'Run2'
-
     name = "truth_%s_fit_%s"%(truth_function, fit_function)
 
     # Open file with fits
-    f = ROOT.TFile("higgsCombine.bias_%s.MultiDimFit.mH120.123456.root"%name)
-    t = f.Get("limit")
+    f = ROOT.TFile("fitDiagnosticsTest.root")
+    t = f.Get("tree_fit_sb")
 
-    hist_pull = ROOT.TH1F("pull_%s"%name, "Pull distribution: truth=%s, fit=%s"%(truth_function, fit_function), 80, -4, 4)
+    hist_pull = ROOT.TH1F("pull_%s"%name, "Pull distribution: truth=%s, fit=%s"%(truth_function, fit_function), 20, -1, 1)
     hist_pull.GetXaxis().SetTitle("Pull = (r_{truth}-r_{fit})/#sigma_{fit}")
     hist_pull.GetYaxis().SetTitle("Entries")
 
@@ -35,21 +24,17 @@ def plot(truth_function, signal_mass, SR, fit_function='dijet2'):
 
     for i_toy in range( N_toys ):
         # Best-fit value
-        t.GetEntry(i_toy*3)
+        t.GetEntry(i_toy)
         r_fit = getattr(t, "r")
+        r_lo = getattr(t, "rLoErr")
+        r_hi = getattr(t, "rHiErr")
 
-        # -1 sigma value
-        t.GetEntry(i_toy*3+1)
-        r_lo = getattr(t, "r")
-
-        # +1 sigma value
-        t.GetEntry(i_toy*3+2)
-        r_hi = getattr(t, "r")
-
-        diff = r_truth-r_fit
+        diff = r_truth - r_fit
         # Use uncertainty depending on where mu_truth is relative to mu_fit
-        if diff > 0: sigma = abs(r_hi-r_fit)
-        else: sigma = abs(r_lo-r_fit)
+        if diff > 0:
+            sigma = abs(r_hi-r_fit)
+        else:
+            sigma = abs(r_lo-r_fit)
 
         if sigma != 0:
             sigma_values = np.append( sigma_values , sigma )
@@ -65,24 +50,30 @@ def plot(truth_function, signal_mass, SR, fit_function='dijet2'):
     # Fit Gaussian to pull distribution
     ROOT.gStyle.SetOptFit(111)
     hist_pull.Fit("gaus")
+    fit = hist_pull.GetFunction("gaus")
 
     canv.SaveAs(f"../plots/fit/{year}/bias_pull_{name}_{signal_mass}_{SR}.pdf")
 
+    return fit.GetParameter(0), fit.GetParameter(1), fit.GetParameter(2)
+
 
 def main():
-    args = parse_commandline()
-    m, SR = args.signal_mass, args.SR
+    A={'expow2':{'SR1':{}, 'SR2':{}}, 'invpow2':{'SR1':{}, 'SR2':{}}}
+    mean={'expow2':{'SR1':{}, 'SR2':{}}, 'invpow2':{'SR1':{}, 'SR2':{}}}
+    sigma={'expow2':{'SR1':{}, 'SR2':{}}, 'invpow2':{'SR1':{}, 'SR2':{}}}
+    
+    for m in [700, 800, 900, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 3000, 3500]:
+        for SR in ['SR1', 'SR2']:
+            os.system(f"combine -M GenerateOnly datacard/Run2/datacard_{m}_{SR}.txt --setParameters pdf_index=1 --toysFrequentist -t {N_toys} --expectSignal 1 --saveToys -m 125 --freezeParameters pdf_index")
+            os.system(f"combine -M FitDiagnostics datacard/Run2/datacard_{m}_{SR}.txt --setParameters pdf_index=0 --toysFile higgsCombineTest.GenerateOnly.mH125.123456.root -t {N_toys} -m 125 --rMin -10 --rMax 10 --freezeParameters pdf_index --cminDefaultMinimizerStrategy=0")
+            A['expow2'][SR][m], mean['expow2'][SR][m], sigma['expow2'][SR][m] = plot(truth_function='expow2', signal_mass=m, SR=SR)
+                        
+            os.system(f"combine -M GenerateOnly datacard/Run2/datacard_{m}_{SR}.txt --setParameters pdf_index=2 --toysFrequentist -t {N_toys} --expectSignal 1 --saveToys -m 125 --freezeParameters pdf_index")
+            os.system(f"combine -M FitDiagnostics datacard/Run2/datacard_{m}_{SR}.txt --setParameters pdf_index=0 --toysFile higgsCombineTest.GenerateOnly.mH125.123456.root -t {N_toys} -m 125 --rMin -10 --rMax 10 --freezeParameters pdf_index --cminDefaultMinimizerStrategy=0")
+            A['invpow2'][SR][m], mean['invpow2'][SR][m], sigma['invpow2'][SR][m] = plot(truth_function='invpow2', signal_mass=m, SR=SR)
 
-    #os.system(f"combine -M GenerateOnly datacard/Run2/{truth_function}/datacard_{m}_{SR}.root -t 1000 -n .generate_{truth_function} --expectSignal 1 --saveToys")
-    #os.system(f"combine -M MultiDimFit datacard/Run2/dijet2/datacard_{m}_{SR}.root -t 1000 -n .bias_truth_{truth_function}_fit_dijet2 --expectSignal 1 --toysFile higgsCombine.generate_{truth_function}.GenerateOnly.mH120.123456.root --algo singles;")
-    
-    os.system(f"combine -M GenerateOnly datacard/Run2/datacard_{m}_{SR}.txt --setParameters pdf_index=1 --toysFrequentist -t 1000 --expectSignal 1 --saveToys -m 125 --freezeParameters pdf_index")
-    os.system(f"combine -M FitDiagnostics datacard/Run2/datacard_{m}_{SR}.txt --setParameters pdf_index=0 --toysFile higgsCombineTest.GenerateOnly.mH125.123456.root -t 1000 -n .bias_truth_expow2_fit_dijet2 --rMin -10 --rMax 10 --freezeParameters pdf_index --cminDefaultMinimizerStrategy=0")
-    plot(truth_function='expow2', signal_mass=m, SR=SR)
-    
-    os.system(f"combine -M GenerateOnly datacard/Run2/datacard_{m}_{SR}.txt --setParameters pdf_index=2 --toysFrequentist -t 1000 --expectSignal 1 --saveToys -m 125 --freezeParameters pdf_index")
-    os.system(f"combine -M FitDiagnostics datacard/Run2/datacard_{m}_{SR}.txt --setParameters pdf_index=0 --toysFile higgsCombineTest.GenerateOnly.mH125.123456.root -t 1000 -n .bias_truth_invpow2_fit_dijet2 --rMin -10 --rMax 10 --freezeParameters pdf_index --cminDefaultMinimizerStrategy=0")
-    plot(truth_function='invpow2', signal_mass=m, SR=SR)
+    with open('./bias_pull.yaml', 'w', encoding='utf-8') as f:
+        yaml.dump({'A': A, 'mean': mean, 'sigma': sigma}, f)
 
 
 if __name__ == "__main__":
