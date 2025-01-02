5c082dcf14312d0377e09b257daf2b954f89baff
Dawei Fu <fudw@pku.edu.cn>
2025-01-02 19:50:29 +0100

    0103

--- fit/test.py
+++ fit/test.py
@@ -1,4 +1,5 @@
-combineTool.py -M Impacts -d datacard/1000/TagHbb.root -m 125 --rMin -1 --rMax 2 --robustFit 1 --doInitialFit --expectSignal 0 -t -1;
-combineTool.py -M Impacts -d datacard/1000/TagHbb.root -m 125 --rMin -1 --rMax 2 --robustFit 1 --doFits --expectSignal 0 -t -1;
-combineTool.py -M Impacts -d  datacard/1000/TagHbb.root -m 125 --rMin -1 --rMax 2 --robustFit 1 --output impacts_Hbb_1000_r0.json --expectSignal 0 -t -1;
-plotImpacts.py -i impacts_Hbb_1000_r0.json -o ../plots/fit/Run2/impacts_Hbb_1000_r0
+text2workspace.py datacard/2000/TagHbb.txt
+combineTool.py -M Impacts -d datacard/2000/TagHbb.root -m 125 --rMin -1 --rMax 2 --robustFit 1 --doInitialFit -t -1;
+combineTool.py -M Impacts -d datacard/2000/TagHbb.root -m 125 --rMin -1 --rMax 2 --robustFit 1 --doFits -t -1;
+combineTool.py -M Impacts -d  datacard/2000/TagHbb.root -m 125 --rMin -1 --rMax 2 --robustFit 1 --output impacts_Hbb_2000.json -t -1;
+plotImpacts.py -i impacts_Hbb_2000.json -o ../plots/fit/Run2/impacts_Hbb_2000
