8b62ddea8a945cea3a8c28b46a922e5e981a6e4a
Dawei Fu <fudw@pku.edu.cn>
2025-01-25 09:10:10 +0100

    fix duplicated events

--- fit/test.py
+++ fit/test.py
@@ -1,5 +1,8 @@
 text2workspace.py datacard/1000/TagHbb.txt
-combineTool.py -M Impacts -d datacard/1000/TagHbb.root -m 125 --rMin -100 --rMax 100 --robustFit 1 -t 1 --expectSignal 0 --doInitialFit;
-combineTool.py -M Impacts -d datacard/1000/TagHbb.root -m 125 --rMin -100 --rMax 100 --robustFit 1 -t 1 --expectSignal 0 --doFits;
-combineTool.py -M Impacts -d datacard/1000/TagHbb.root -m 125 --rMin -100 --rMax 100 --robustFit 1 -t 1 --expectSignal 0 --output impacts_Hbb_1000.json;
-plotImpacts.py -i impacts_Hbb_1000.json -o ../plots/fit/Run2/impacts_Hbb_1000
+combineTool.py -M Impacts -d datacard/1000/TagHbb.root -m 125 --rMin -1 --rMax 1 --robustFit 1 -t 1 --expectSignal 0 --doInitialFit;
+python3 work.py;
+combineTool.py -M Impacts -d datacard/1000/TagHbb.root -m 125 --rMin -1 --rMax 1 --robustFit 1 -t 1 --expectSignal 0 --doFits;
+python3 work.py;
+combineTool.py -M Impacts -d datacard/1000/TagHbb.root -m 125 --rMin -1 --rMax 1 --robustFit 1 -t 1 --expectSignal 0 --output impacts_Hbb_2000_r0_t1.json;
+python3 work.py;
+plotImpacts.py -i impacts_Hbb_2000_r0_t1.json -o ../plots/fit/Run2/impacts_Hbb_2000_r0_t1
---
+++ fit/work.py
@@ -0,0 +1,4 @@
+import os
+for i in os.listdir('.'):
+    if '123456' in i:
+        os.system(f"mv {i} {i.replace('.123456', '')}")
