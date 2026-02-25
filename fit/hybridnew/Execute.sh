7cb67e9188faf39c783c59cb3f78d4a5e0eb4eb5
Dawei Fu <fudw@pku.edu.cn>
2026-02-25 10:18:39 +0100

    start hybridnew

--- fit/hybridnew/Execute.sh
+++ fit/hybridnew/Execute.sh
@@ -4,7 +4,7 @@ cd CMSSW_14_1_0_pre4/src
 cmsenv
 git -c advice.detachedHead=false clone --depth 1 --branch v10.2.1 https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 cd HiggsAnalysis/CombinedLimit
-scramv1 b clean; scramv1 b # always make a clean build
+scramv1 b clean; scramv1 b -j$(nproc --ignore=2) # always make a clean build, with n - 2 cores on the system
 
 cd ../../../..
 
